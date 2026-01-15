7cdc04717f83f0317e34bcac6e55f0268af29a2c
Emmanuella N. D. <57154645+EmmaDjeufa@users.noreply.github.com>
2026-01-15 14:59:23 +0000

    V1

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,18 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken')
 
-module.exports = function (req, res, next) {
-  const authHeader = req.headers.authorization
-  if (!authHeader) {
-    return res.status(401).json({ message: 'Token manquant' })
-  }
+module.exports = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  if (!authHeader) return res.status(401).json({ message: 'Token manquant' })
 
   const token = authHeader.split(' ')[1]
+  if (!token) return res.status(401).json({ message: 'Token invalide' })
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = decoded
+    req.user = { id: decoded.id, role: decoded.role }
     next()
   } catch (err) {
-    return res.status(401).json({ message: 'Token invalide' })
+    console.error('JWT ERROR:', err)
+    res.status(401).json({ message: 'Token invalide' })
   }
 }
