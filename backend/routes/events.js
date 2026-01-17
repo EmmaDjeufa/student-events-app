a8a352ea8b2777fe02048c69a40405793df9e7ac
Emmanuella N. D. <57154645+EmmaDjeufa@users.noreply.github.com>
2026-01-17 14:03:29 +0000

    fix cloudinary avatar upload

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -7,7 +7,7 @@ const controller = require('../controllers/eventController')
 router.get('/', controller.getAllEvents)
 router.get('/:id', controller.getEventById)
 
-router.post('/', auth, admin, controller.createEvent)
+router.post('/', auth, controller.createEvent)
 router.put('/:id', auth, admin, controller.updateEvent)
 router.delete('/:id', auth, admin, controller.deleteEvent)
 
