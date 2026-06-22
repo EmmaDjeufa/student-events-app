0569d8b117d8e6a2f31f363b243302667c8b6784
Emmanuella N. D. <57154645+EmmaDjeufa@users.noreply.github.com>
2026-06-22 13:34:41 +0000

    migrate database to supabase

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -36,17 +36,7 @@ function Events() {
   return (
     <div className="events-container">    
       <h1 className="events-title">Événements</h1>
-      <p className="demo-text">
-        Le backend est hébergé sur Render (PostgreSQL). Service arrêté ? En attendant son renouvellement —— voici une démo vidéo du projet en fonctionnement :  
-        <a 
-          href="https://youtu.be/wAm77QudYA8" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="youtube-link"
-        >
-          Voir la vidéo YouTube 🎬
-        </a>
-      </p>
+     
 
       {/* Barre de recherche */}
       <input
--- frontend/src/pages/Registrations.jsx
+++ frontend/src/pages/Registrations.jsx
@@ -41,17 +41,7 @@ function Registrations() {
   return (
     <div className="registrations-page">
       <h1>Liste des utilisateurs</h1>
-      <p className="demo-text">
-        Le backend est hébergé sur Render (PostgreSQL). Service arrêté ? En attendant son renouvellement —- voici une démo vidéo du projet en fonctionnement :  
-        <a 
-          href="https://youtu.be/wAm77QudYA8" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="youtube-link"
-        >
-          Voir la vidéo YouTube 🎬
-        </a>
-      </p>
+      
       {!token && (
         <p className="notice">
           Connectez-vous pour voir les informations complètes des utilisateurs !
